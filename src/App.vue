ef38d4c7b3ed71cbc82959428db4062d5c3b7aee
Evan Vosh <evanvosh@MacBook-Air-Evan.local>
2023-07-07 09:25:25 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+
+  <div class="flex justify-center pt-16">
+    <input type="text" 
+          placeholder=" Write a movie name" 
+          v-bind:value="inputText"
+          v-on:input="inputChangeHandler"
+    />
+  </div>
+  <div class = "flex justify-center pt-10">
+    <button v-on:click="searchMovie" class="box-border h-11 w-40 border-4">Search</button>
+
+  <div>
+    <div class="bg-blue-400 w-32 h-64 m-4 p-8">
+      {{ apiData }}
+    </div>
+    <div v-if="inputText != ''" class="bg-blue-400 w-32 h-64 m-4 p-8">
+      {{ moviePlot }}
+    </div>
+  </div>
+  </div>
+      
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import { toHandlers } from 'vue';
+
+export default {
+  data() {
+    return {
+      apiData: [],
+      inputText: '',
+      moviePlot: '',
+    }
+  },
+  methods: {
+      inputChangeHandler(event) {
+        this.inputText = event.target.value;
+        axios
+        .get(`http://www.omdbapi.com/?t=${this.inputText}&apikey=2b1b57fb`)
+        .then(response => this.apiData = response.data.Title)
+        .catch(error => {
+            console.log(error);
+        })
+        if (this.inputText == ''){
+          this.moviePlot = '';
+        }
+
+      },
+      searchMovie() {
+        axios
+        .get(`http://www.omdbapi.com/?t=${this.inputText}&apikey=2b1b57fb`)
+        .then(response => this.moviePlot = response.data.Plot)
+        .catch(error => {
+            console.log(error);
+        })
+        
+      }
+
+    }
+   
+} 
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/dd.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+  
+    <div class="showMovies">
+    
+      <ul>
+        <li class="bg-blue-400 w-32 h-64 m-4 p-8" v-for="name in apiData">
+          {{ name }}
+         </li>
+      </ul>
+    </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        apiData: [],
+      }
+    },
+    mounted() {
+      let movieNames = ['avengers', 'cruella'];
+      for (let movieName of movieNames){
+        axios
+          .get(`http://www.omdbapi.com/?t=${movieName}&apikey=2b1b57fb`)
+          .then(response => this.apiData.push(response.data.Title))
+          .catch(error => {
+              console.log(error)
+          })
+  
+      }
+  
+    },
+    methods: {
+  
+  
+      }
+     
+  } 
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
